7fc5a2e3be1ad5dc5c83725f6ec1a3099070970d
DEDZTBH <peiqial@outlook.com>
2019-04-27 22:49:55 -0400

    update webcam.py

--- demo/webcam.py
+++ demo/webcam.py
@@ -1,18 +1,22 @@
+import time
 from os import path
 
-import face_recognition
 import cv2
+import face_recognition
+
+# For windows
+import os
+import sys
+sys.path.append(os.getcwd())
 
 from knn import predict
 from util.general import load
 
-frame_scale = 0.5
-knn_pkl_name = 'knn_1719_preprocess_test_0_100_neq_2'
+frame_scale = 1
+knn_pkl_name = 'knn_1719_preprocess_0_100_neq_2'
 webcam = 0
 skip_frame = True
-confirm_frames = 10
-
-confirm_frames = int(confirm_frames / (2 if skip_frame else 1))
+confirm_time_ms = 1500
 
 # Get a reference to webcam #0 (the default one)
 video_capture = cv2.VideoCapture(webcam)
@@ -29,56 +33,24 @@ knn_model = load(knn_pkl_name, folder=path.join('data', 'model', 'knn'))
 
 
 def confirm_op(names):
+    current_time = time.time()
     for n in [x for x in names if x != 'Unknown']:
         if n not in confirming:
-            confirming[n] = 0
-        confirming[n] += 1
-        if confirming[n] >= confirm_frames:
+            confirming[n] = current_time
+        if (current_time - confirming[n]) >= confirm_time_ms:
             if n not in confirmed:
                 confirmed.append(n)
 
     for k in confirming:
         if k not in names:
-            confirming[k] = 0
+            confirming[k] = current_time
 
-
-# def average_frames(frames):
-#     (rAvg, gAvg, bAvg) = (None, None, None)
-#     total = 0
-#     for frame in frames:
-#         (B, G, R) = cv2.split(frame.astype("float"))
-#         # if the frame averages are None, initialize them
-#         if rAvg is None:
-#             rAvg = R
-#             bAvg = B
-#             gAvg = G
-#
-#         # otherwise, compute the weighted average between the history of
-#         # frames and the current frames
-#         else:
-#             rAvg = ((total * rAvg) + (1 * R)) / (total + 1.0)
-#             gAvg = ((total * gAvg) + (1 * G)) / (total + 1.0)
-#             bAvg = ((total * bAvg) + (1 * B)) / (total + 1.0)
-#
-#         # increment the total number of frames read thus far
-#         total += 1
-#     avg = cv2.merge([bAvg, gAvg, rAvg]).astype("uint8")
-#     return avg
-
-
-# prev_frame = None
 
 while True:
     # Grab a single frame of video
     ret, frame = video_capture.read()
 
-    # if prev_frame is None:
-    #     prev_frame = frame
-    # else:
-    #     frame = average_frames([prev_frame, frame])
-    #     prev_frame = None
-
-    # start = time.time()
+    start = time.time()
     # Resize frame of video to xxx size for faster face recognition processing
     small_frame = cv2.resize(frame, (0, 0), fx=frame_scale, fy=frame_scale)
 
@@ -88,12 +60,12 @@ while True:
     # Only process every other frame of video to save time
     if process_this_frame:
         # Find all the faces and face encodings in the current frame of video
-        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_locations = face_recognition.face_locations(rgb_small_frame, model='cnn')
         face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
         face_names = predict(face_encodings,
                              knn_model,
-                             distance_threshold=0.46,
+                             distance_threshold=0.52,
                              n_neighbors=2,
                              print_time=False)
 
